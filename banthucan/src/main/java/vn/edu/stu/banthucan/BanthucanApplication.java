ec0bb35a26e7587bc0170a8ce59eea12be1f21a5
NQH1611 <quochuy.140101@gmail.com>
2023-04-01 14:05:27 +0700

    create project

---
+++ banthucan/src/main/java/vn/edu/stu/banthucan/BanthucanApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.stu.banthucan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanthucanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanthucanApplication.class, args);
+	}
+
+}
